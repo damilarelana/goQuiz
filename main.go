643c39fd996f42b264601a1c5715b5e5e3ca0cf7
Damilare Lana <damilarelana@gmail.com>
2020-03-04 23:13:48 +0100

    added errMsgHandler for readers. utilized csv packages NewReader method to generate a reader method, from which we can then parse the CSV content

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "flag"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func errMsgHandler(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
 
 func main() {
-	csvFilename := flag.String("csv", "quizData.csv", "a csv file containing the quiz's question and answer data, in the format 'question, answer'")
+	csvFilename := flag.String("csv", "quizData.csv", "a csv file containing quiz's question and answer data, in the format 'question, answer'")
 	flag.Parse() // require to initialize the specified flags with the Operating system
+
+	openedFile, err := os.Open(*csvFilename)
+	if err != nil {
+		errMsgHandler(fmt.Sprintf("Failed to open CSV file: %s\n", *csvFilename))
+	}
+	r := csv.NewReader(openedFile)
+
 }
