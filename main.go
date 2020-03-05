517cd5be6f98b837bc2eb48d2ec90c8d54814ee4
Damilare Lana <damilarelana@gmail.com>
2020-03-06 00:38:10 +0100

    csv flags working. able to open and read from csv file

--- main.go
+++ main.go
@@ -15,15 +15,20 @@ func errMsgHandler(msg string) {
 func main() {
 
 	// define flags
-	csvFilename := flag.String("csv", "quizData.csv", "a csv file containing quiz's question and answer data, in the format 'question, answer'")
+	csvFilename := flag.String("csv", "quizData.csv", "a csv file containing question/answer data, in a 'question, answer' format per record line")
 
 	// parse flags
 	flag.Parse() // required to initialize the specified flags with the Operating system
 
+	// open CSV file and read the records (i.e. all the lines) from it
 	openedFile, err := os.Open(*csvFilename)
 	if err != nil {
 		errMsgHandler(fmt.Sprintf("Failed to open CSV file: %s\n", *csvFilename))
 	}
 	r := csv.NewReader(openedFile)
-
+	records, err := r.ReadAll()
+	if err != nil {
+		errMsgHandler("Failed to parse the provided CSV file")
+	}
+	fmt.Println(records)
 }
